25827e47f49420a02657c121eda30e4138527bef
Arun KC <arunkc91@gmail.com>
2022-03-01 21:36:51 +0530

    fix: header import in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Main from './components/Header/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Header />
     </div>
   );
 }
