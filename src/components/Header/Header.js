fa27957c7b128978de77a894729bd1a2e0f6b2f0
Arun KC <arunkc91@gmail.com>
2022-03-05 07:51:03 +0530

    fix: metamask install popup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,9 +25,23 @@ export default function Header(props) {
     )
   } else {
     return (
-      <header>
-        <h1>Please install metamask</h1>
+
+
+      <header className="h-20 mt-2 px-5 flex items-center">
+        <img className="h-full" src={icon} alt="icon"></img>
+        <h1 className="mr-auto font-semibold justify-content-center text-3xl">
+          byteHash
+        </h1>
+        <div className='backdrop-filter z-10 backdrop-blur-sm absolute inset-0
+                        flex justify-center items-center text-left'>
+          <a className="bg-[#4c249f] p-6 border-2 w-1/3 text-center rounded-xl flex justify-center cursor-pointer"
+          href="https://metamask.io/" target="_blank" rel="noreferrer">
+                  <h1 className="text-lg font-bold">Please install metamask</h1>
+          </a>
+
+        </div>
       </header>
+        
     )
   }
 }
